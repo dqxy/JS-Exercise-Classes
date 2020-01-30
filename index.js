cd5e7c83e133f74cda016d7c1846edd4bc611047
spore <spore@spores-MBP.fios-router.home>
2020-01-30 16:20:18 -0500

    JS

--- index.js
+++ index.js
@@ -39,11 +39,30 @@ class Airplane {
     - Give instances of Person a method `.toString()`:
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
-
+var a = 0;
 class Person {
-
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  
+  eat() {
+    if (this.stomach.length <= 9) {
+      a++;
+      this.stomach.push(a);
+    } else {
+      a = 9;
+      this.stomach = [1,2,3,4,5,6,7,8,9,10];
+    }
+  }
+  poop() {
+    this.stomach = [];
+  }
+  toString(){
+    return `${this.name} , ${this.age}`;
+  }
 }
-
 /*
   TASK 2
     - Write a Car class whose constructor initializes `model` and `milesPerGallon` from arguments.
@@ -59,7 +78,29 @@ class Person {
 */
 
 class Car {
-
+  constructor(model,milesPerGallon){
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+  }
+  fill(gallons){
+    this.tank = this.tank + gallons;
+  }
+  drive(distance){
+    
+    
+    
+ 
+      if(this.tank - (distance/this.milesPerGallon) > 0 )
+        {this.odometer+=distance;
+        this.tank = this.tank - (distance/this.milesPerGallon);}
+      else{ 
+      this.odometer+=distance-1;
+      this.tank=0;
+      return `I ran out of fuel at ${this.odometer} miles!`;}
+    
+  }
 }
 
 /*
@@ -75,8 +116,15 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
-}
+    constructor(attributes) {
+      this.name = attributes.name;
+      this.age=attributes.age;
+      this.location=attributes.location;
+    }
+    speak() {
+      return `Hello my name is ${this.name}, I am from ${this.location}`;
+    }
+  }
 
 /*
   TASK 4
@@ -92,8 +140,19 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian{
+  constructor(attributes){
+    super(attributes)// this replaces parent.call
+    this.specialty = attributes.specialty;
+    this.favLanguage=attributes.favLanguage;
+    this.catchPhrase=attributes.catchPhrase;
+  }// this is where any specific methods of the child will go
+    demo(subject){
+      return `Today we are learning about ${subject}`;
+    }
+    grade(student, subject){
+      return `${student.name} receives a perfect score on ${subject}`;
+    }
 }
 
 /*
@@ -111,8 +170,22 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian{
+  constructor(attributes){
+    super(attributes)// this replaces parent.call
+    this.previousBackground = attributes.previousBackground;
+    this.className=attributes.className;
+    this.favSubjects=attributes.favSubjects;
+  }// this is where any specific methods of the child will go
+  listSubjects(){
+      return this.favSubjects.toString();
+    }
+    PRAssignment(subject){
+      return `${this.name} has submitted a PR for ${subject}`;
+    }
+    sprintChallenge(subject){
+      return `${this.name} has begun sprint challenge on ${subject}`;
+    }
 }
 
 /*
@@ -128,8 +201,18 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
+class ProjectManager extends Instructor{
+  constructor(attributes){
+    super(attributes)// this replaces parent.call
+    this.gradClassName = attributes.gradClassName;
+    this.favInstructor=attributes.favInstructor;
+  }// this is where any specific methods of the child will go
+  standUp(channel){
+      return `${this.name} announces to ${channel}, @channel standy times!`;
+    }
+    debugsCode(student,subject){
+      return `${this.name} debugs ${student.name}'s code on ${subject}`;
+    }
 }
 
 /*
